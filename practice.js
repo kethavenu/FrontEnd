323988db273dfe273eb5fa8470c5d5a38b0d85c6
kethavenu6032 <kethavenu6032@gmail.com>
2024-01-29 23:39:08 +0530

    Adding first practice project

---
+++ practice.js
@@ -0,0 +1,5 @@
+function changeColorOfHeading() {
+  document.getElementById("MainHeading").textContent =
+    "Hi, Staring new project";
+  document.getElementById("MainHeading").style.color = "Blue";
+}
